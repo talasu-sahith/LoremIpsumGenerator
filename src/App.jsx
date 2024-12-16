c0c21104dabdc647d6fa0c9272952f5eb003fbf6
talasu-sahith <sahithtalasu.123@gmail.com>
2024-12-17 00:11:20 +0530

    generator using Filter function

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,45 @@
+import { useState } from "react";
+import data from "./data";
 const App = () => {
-  return <h2>Lorem Ipsum Starter</h2>;
+  const [count, setCount] = useState(1);
+  const [text, setText] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setText((prev) => {
+      var newtext = [];
+      newtext = data.filter((item, index) => count > index);
+      return newtext;
+    });
+  };
+  console.log(text);
+  return (
+    <section className="section-center">
+      <h4>Tired of boring lorem ipsum</h4>
+      <form className="lorem-form" onSubmit={handleSubmit}>
+        <label htmlFor="amount">Paragraphs :</label>
+        <input
+          type="number"
+          id="amount"
+          name="amount"
+          min="1"
+          step="1"
+          max="8"
+          value={count}
+          onChange={(e) => setCount(e.target.value)}
+        ></input>
+        <button className="btn" type="submit">
+          generate
+        </button>
+      </form>
+      {text.map((item, index) => {
+        return (
+          <div className="lorem-text" key={index}>
+            <p>{item}</p>
+          </div>
+        );
+      })}
+    </section>
+  );
 };
 export default App;
